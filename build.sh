613ecc9204b9f9050456183f8645f1cc68535a8c
TidalMist <91356643+TidalMist@users.noreply.github.com>
2025-05-22 01:35:17 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,10 +15,15 @@ rm -rf patched/META-INF
 
 sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">#ff000000<\/color>/g' patched/res/values-night/colors.xml
 sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' patched/res/values-night/colors.xml
-sed -i 's/ff1c1b22/ff000000/g' patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff2b2a33/ff000000/g' patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff42414d/ff15141a/g' patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff52525e/ff15141a/g' patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
+sed -i 's/<color name="photonDarkGrey05">.*/<color name="photonDarkGrey05">#ff25242b<\/color>/g' patched/res/values/colors.xml
+sed -i 's/<color name="button_material_dark">.*/<color name="button_material_dark">#ff25242b<\/color>/g' patched/res/values/colors.xml
+sed -i 's/ff1c1b22/ff000000/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
+sed -i 's/ff2b2a33/ff000000/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
+sed -i 's/ff42414d/ff15141a/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
+sed -i 's/ff52525e/ff23212b/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
+sed -i 's/ff5b5b66/ff2b2936/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
+sed -i 's/1c1b22/000000/g' patched/assets/extensions/readerview/readerview.css
+sed -i 's/eeeeee/e3e3e3/g' patched/assets/extensions/readerview/readerview.css
 
 ./apktool b patched -o patched.apk --use-aapt2
 
